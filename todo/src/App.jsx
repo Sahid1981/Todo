0035f7ee6a3403e2414b94039e09fdae7eded3f8
Juha Jermalainen <juha.jermalainen@gmail.com>
2026-08-25 01:35:57 +0300

    end of the day.

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -3,6 +3,16 @@ import './App.css'
 function App() 
   const [task, setState] = useState('')
   const [tasks, setTasks] = useState([])
+  const addTask = (event) => {
+    event.preventDefault()
+    const description = task.trim()
+    if (!description) return
+    setTasks(currentTasks => [...currentTasks, description])
+    setTask('')
+  }
+  const deleyteTask = (delete) => {
+    setTasks(currentTasks => currentTasks.filter(item => item !== delete))
+  }
   return (
     <div id="container">
       <h3>Todos</h3>
@@ -12,7 +22,13 @@ function App()
           onChange={event => setTask(event.target.value)}
         />
       </form>
-      <ul></ul>
+      <ul>
+        {
+          tasks.map(item => (
+            <li key={item}>{item}</li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
